c12a808ab64f41f1c6f2d02b909117b7acba4bf0
Muzammil Momin <muzammilmomin@live.com>
2018-10-26 23:32:15 +0530

    Auto-committing 'deploy.sh'

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t muzammilmomin/multi-client:latest -t muzammilmomin/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t muzammilmomin/multi-server -f ./server/Dockerfile ./server
+docker build -t muzammilmomin/multi-server:latest -t muzammilmomin/mutli-server:$SHA -f ./server/Dockerfile ./server
 docker build -t muzammilmomin/multi-worker -f ./worker/Dockerfile ./worker
 docker push muzammilmomin/multi-client
 docker push muzammilmomin/multi-server
